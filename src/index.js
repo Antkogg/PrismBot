03105e81aba6789bac44e566f5694cc9f6fc6c58
Antkogg <antkogg@gmail.com>
2026-07-03 00:22:39 -0600

    Remove social media integration per user request

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { env } from './config/env.js';
 import { loadCommands } from './handlers/commandHandler.js';
 import { loadEvents } from './handlers/eventHandler.js';
 import { setupErrorHandlers } from './handlers/errorHandler.js';
-import { initSocialMediaPolling } from './services/socialMediaService.js';
 import { initDatabase } from './database/db.js';
 import logger from './utils/logger.js';
 
@@ -36,8 +35,6 @@ const init = async () => {
         await client.login(env.BOT_TOKEN);
         
         // Start background tasks once logged in
-        initSocialMediaPolling(client);
-        
     } catch (error) {
         logger.error('Failed to log in to Discord. Check your BOT_TOKEN.', error);
     }
